c77d639e875e765498f770e179f312b97bd26ccb
Fabio Zahner <zahnerfabio@gmail.com>
2021-09-10 16:15:11 +0200

    removed duplicate file

--- projects/lib/theme/themes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-// TODO edit themes, maybe add more (such as high contrast)
-
-final ThemeData lightTheme = new ThemeData.light();
-final ThemeData darkTheme = new ThemeData.dark();
