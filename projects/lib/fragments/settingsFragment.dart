dad7e993be43e74c6544fdc198ff6d3a89eee354
Fabio Zahner <zahnerfabio@gmail.com>
2021-09-10 11:52:59 +0200

    Implemented Theme Switching with persistence

--- projects/lib/fragments/settingsFragment.dart
+++ projects/lib/fragments/settingsFragment.dart
@@ -1,15 +1,27 @@
 import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsFragment extends StatelessWidget {
-
   static SharedPreferences? prefs;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement settings
     // TODO maybe multi language support
+
+    ThemeMode? themeMode = EasyDynamicTheme.of(context).themeMode;
+    String dropdownValue;
+    if(themeMode == ThemeMode.light) {
+      dropdownValue = "Light";
+    } else if (themeMode == ThemeMode.dark) {
+      dropdownValue = "Dark";
+    } else if (themeMode == ThemeMode.system) {
+      dropdownValue = "Use System Theme";
+    } else {
+      throw("App theme could not be determined");
+    }
     return new Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -19,60 +31,43 @@ class SettingsFragment extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Padding(padding: EdgeInsets.all(10),
+              Padding(
+                padding: EdgeInsets.all(10),
                 child: Text("App Theme"),
               ),
-              Padding(padding: EdgeInsets.all(10),
-                child: FutureBuilder(
-                  future: getSharedPreferences('theme'),
-                  builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                    if(snapshot.hasData){
-                      String snapshotValue = snapshot.data;
-                      if(snapshotValue == "no value") {
-                        snapshotValue = 'Light';
-                        setSharedPreferences('theme', 'Light');
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: DropdownButton<String>(
+                    // TODO 3rd option: Use system theme
+                    value: dropdownValue,
+                    onChanged: (String? newValue) {
+                      if (newValue == "Light") {
+                        EasyDynamicTheme.of(context).changeTheme(dark: false);
+                      } else if (newValue == "Dark") {
+                        EasyDynamicTheme.of(context).changeTheme(dark: true);
+                      } else if (newValue == 'Use System Theme') {
+                        EasyDynamicTheme.of(context).changeTheme(dynamic: true);
+                      } else {
+                        throw ("Theme string could not be read");
                       }
-                      return DropdownButton<String>( // TODO 3rd option: Use system theme
-                        value: snapshotValue,
-                        onChanged: (String? newValue) {
-                          if(newValue == "Light") {
-                            EasyDynamicTheme.of(context).changeTheme(dark: false);
-                          } else if (newValue == "Dark") {
-                            EasyDynamicTheme.of(context).changeTheme(dark: true);
-                          } else if (newValue == 'Use System Theme'){
-                            EasyDynamicTheme.of(context).changeTheme(dynamic: true);
-                          } else {
-                            throw("Theme string could not be read");
-                          }
-                          snapshotValue = newValue!;
-                          setSharedPreferences('theme', newValue);
-                        },
-                        icon: const Icon(Icons.arrow_downward),
-                        iconSize: 24,
-                        elevation: 16,
-                        underline: Container(
-                          height: 2,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        items: <String>['Light', 'Dark', 'Use System Theme']
-                          .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
+                      dropdownValue = newValue!;
+                    },
+                    icon: const Icon(Icons.arrow_downward),
+                    iconSize: 24,
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    items: <String>['Light', 'Dark', 'Use System Theme']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
                       );
-                    } else {
-                      return DropdownButton(
-                        items: null,
-                        onChanged: null,
-                        disabledHint: Text("Loading preferences"),
-                      );
-                    }
-                  },
-                )
-
-              ),
+                    }).toList(),
+                  )
+              )
             ],
           )
         ],
@@ -89,11 +84,10 @@ class SettingsFragment extends StatelessWidget {
   Future<String> getSharedPreferences(String key) async {
     prefs = await SharedPreferences.getInstance();
     String? key = prefs!.getString('stringValue');
-    if(key != null){
+    if (key != null) {
       return key;
-    }else {
-     return "no value";
+    } else {
+      return "no value";
     }
   }
-
 }
