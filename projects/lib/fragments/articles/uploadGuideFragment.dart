068c5b938e6713254516a6ae9bbf124d44c1885d
Fabio Zahner <zahnerfabio@gmail.com>
2021-09-13 08:56:00 +0200

    created unorderedListItem.dart

--- projects/lib/fragments/articles/uploadGuideFragment.dart
+++ projects/lib/fragments/articles/uploadGuideFragment.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:projects/style/textStyles.dart' as customStyles;
+import 'package:projects/style/unorderedListItem.dart';
 
 // From https://commons.wikimedia.org/wiki/Commons:First_steps/Uploading_files
 // and https://commons.wikimedia.org/wiki/File:Licensing_tutorial_en.svg
@@ -93,42 +94,6 @@ class HeroPhotoViewRouteWrapper extends StatelessWidget {
         maxScale: maxScale,
         heroAttributes: const PhotoViewHeroAttributes(tag: "someTag"),
       ),
-    );
-  }
-}
-
-class UnorderedList extends StatelessWidget {
-  UnorderedList(this.texts);
-  final List<String> texts;
-
-  @override
-  Widget build(BuildContext context) {
-    var widgetList = <Widget>[];
-    for (var text in texts) {
-      // Add list item
-      widgetList.add(UnorderedListItem(text));
-      // Add space between items
-      widgetList.add(SizedBox(height: 5.0));
-    }
-
-    return Column(children: widgetList);
-  }
-}
-
-class UnorderedListItem extends StatelessWidget {
-  UnorderedListItem(this.text);
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("• "),
-        Expanded(
-          child: Text(text),
-        ),
-      ],
     );
   }
 }
---
+++ projects/lib/style/unorderedListItem.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+
+class UnorderedList extends StatelessWidget {
+  UnorderedList(this.texts);
+  final List<String> texts;
+
+  @override
+  Widget build(BuildContext context) {
+    var widgetList = <Widget>[];
+    for (var text in texts) {
+      // Add list item
+      widgetList.add(UnorderedListItem(text));
+      // Add space between items
+      widgetList.add(SizedBox(height: 5.0));
+    }
+
+    return Column(children: widgetList);
+  }
+}
+
+class UnorderedListItem extends StatelessWidget {
+  UnorderedListItem(this.text);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("• "),
+        Expanded(
+          child: Text(text),
+        ),
+      ],
+    );
+  }
+}
