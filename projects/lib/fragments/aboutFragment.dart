fdbdddf1cd4cb12ac29a7c6a9dc712698d5ba634
Remo Steiner <remo.steiner@ost.ch>
2021-09-30 13:53:42 +0200

    optimized darkmode

--- projects/lib/fragments/aboutFragment.dart
+++ projects/lib/fragments/aboutFragment.dart
@@ -1,3 +1,4 @@
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:projects/style/textStyles.dart';
@@ -6,40 +7,86 @@ import 'package:projects/style/unorderedListWidget.dart';
 class AboutFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("About"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("Wikimedia Commons Uploader App", style: fragmentTitle),
-              Padding(padding: EdgeInsets.only(bottom: 8)),
-              Text("Developed by:", style: articleDescription),
-              Padding(padding: EdgeInsets.only(bottom: 4)),
-              Text("• Fabio Zahner"),
-              Text("• Remo Steiner"),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: SizedBox(
-                  width: 300,
-                  child: Image.asset("assets/media/logos/IFS.png"), // TODO Bc the logos are transparent, they are not readable in dark mode
-                ),
+    ThemeMode? themeMode = EasyDynamicTheme.of(context).themeMode;
+    if (themeMode == ThemeMode.light) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("About"),
+        ),
+        body: Center(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Wikimedia Commons Uploader App", style: fragmentTitle),
+                  Padding(padding: EdgeInsets.only(bottom: 8)),
+                  Text("Developed by:", style: articleDescription),
+                  Padding(padding: EdgeInsets.only(bottom: 4)),
+                  Text("• Fabio Zahner"),
+                  Text("• Remo Steiner"),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: SizedBox(
+                      width: 300,
+                      child: Image.asset(
+                          "assets/media/logos/IFS.png"),
+
+
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: SizedBox(
+                      width: 220,
+                      child: Image.asset("assets/media/logos/OST.png"),
+                    ),
+                  )
+                ],
+
               ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: SizedBox(
-                  width: 220,
-                  child: Image.asset("assets/media/logos/OST.png"),
-                ),
-              )
-            ],
-          ), 
-        )
-      ),
-    );
+            )
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("About"),
+        ),
+        body: Center(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Wikimedia Commons Uploader App", style: fragmentTitle),
+                  Padding(padding: EdgeInsets.only(bottom: 8)),
+                  Text("Developed by:", style: articleDescription),
+                  Padding(padding: EdgeInsets.only(bottom: 4)),
+                  Text("• Fabio Zahner"),
+                  Text("• Remo Steiner"),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                    child: SizedBox(
+                      width: 380,
+                      child: Image.asset(
+                          "assets/media/logos/IFS_fordarkmode.png"),
+
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                    child: SizedBox(
+                      width: 300,
+                      child: Image.asset("assets/media/logos/OST_fordarkmode.png"),
+                    ),
+                  )
+                ],
+
+              ),
+            )
+        ),
+      );
+    }
   }
 }
