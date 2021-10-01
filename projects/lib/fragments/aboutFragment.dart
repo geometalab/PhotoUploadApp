ea90ac44159f385e7ae001fc6fa3318ffa593ffa
Fabio Zahner <zahnerfabio@gmail.com>
2021-10-01 08:38:51 +0200

    made aboutFragment.dart a bit more respectable code-wise

--- projects/lib/fragments/aboutFragment.dart
+++ projects/lib/fragments/aboutFragment.dart
@@ -5,88 +5,53 @@ import 'package:projects/style/textStyles.dart';
 import 'package:projects/style/unorderedListWidget.dart';
 
 class AboutFragment extends StatelessWidget {
+  late Image ifsLogo;
+  late Image ostLogo;
+
   @override
   Widget build(BuildContext context) {
     ThemeMode? themeMode = EasyDynamicTheme.of(context).themeMode;
-    if (themeMode == ThemeMode.light) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("About"),
-        ),
-        body: Center(
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text("Wikimedia Commons Uploader App", style: fragmentTitle),
-                  Padding(padding: EdgeInsets.only(bottom: 8)),
-                  Text("Developed by:", style: articleDescription),
-                  Padding(padding: EdgeInsets.only(bottom: 4)),
-                  Text("• Fabio Zahner"),
-                  Text("• Remo Steiner"),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: SizedBox(
-                      width: 300,
-                      child: Image.asset(
-                          "assets/media/logos/IFS.png"),
-
-
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: SizedBox(
-                      width: 220,
-                      child: Image.asset("assets/media/logos/OST.png"),
-                    ),
-                  )
-                ],
-
-              ),
-            )
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("About"),
-        ),
-        body: Center(
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text("Wikimedia Commons Uploader App", style: fragmentTitle),
-                  Padding(padding: EdgeInsets.only(bottom: 8)),
-                  Text("Developed by:", style: articleDescription),
-                  Padding(padding: EdgeInsets.only(bottom: 4)),
-                  Text("• Fabio Zahner"),
-                  Text("• Remo Steiner"),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: SizedBox(
-                      width: 380,
-                      child: Image.asset(
-                          "assets/media/logos/IFS_fordarkmode.png"),
-
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: SizedBox(
-                      width: 300,
-                      child: Image.asset("assets/media/logos/OST_fordarkmode.png"),
-                    ),
-                  )
-                ],
-
-              ),
-            )
-        ),
-      );
+    if(themeMode == ThemeMode.dark){ // TODO Recognize "system default" color
+      ifsLogo = Image.asset("assets/media/logos/IFS_dark.png");
+      ostLogo = Image.asset("assets/media/logos/OST_dark.png");
+    }else{
+      ifsLogo = Image.asset("assets/media/logos/IFS.png");
+      ostLogo = Image.asset("assets/media/logos/OST.png");
     }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About"),
+      ),
+      body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Wikimedia Commons Uploader App", style: fragmentTitle),
+                Padding(padding: EdgeInsets.only(bottom: 8)),
+                Text("Developed by:", style: articleDescription),
+                Padding(padding: EdgeInsets.only(bottom: 4)),
+                Text("• Fabio Zahner"),
+                Text("• Remo Steiner"),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 300,
+                    child: ifsLogo,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: 220,
+                    child: ostLogo,
+                  ),
+                )
+              ],
+            ),
+          )
+      ),
+    );
   }
 }
