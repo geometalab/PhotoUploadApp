4412c249c36ddcc724048d463426858f283f21a9
Fabio Zahner <zahnerfabio@gmail.com>
2021-10-26 14:52:51 +0200

    created class CustomColors in themes.dart for Colors which are reused throughout the app but dont have a applicable category in the Theme.

--- projects/lib/style/themes.dart
+++ projects/lib/style/themes.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = new ThemeData.light().copyWith();
 final ThemeData darkTheme = new ThemeData.dark().copyWith();
+
+class CustomColors {
+  static const WARNING_COLOR = Colors.amber;
+  static const NO_IMAGE_COLOR = Colors.grey;
+}
