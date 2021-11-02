4b5063c7e68d879c93f08d23b7ffa1f92fc0deb8
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2021-11-02 11:16:11 +0100

    outsourced HeroPhotoViewRouteWrapper.dart for reuse

--- projects/lib/fragments/articles/uploadGuideFragment.dart
+++ projects/lib/fragments/articles/uploadGuideFragment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:projects/style/HeroPhotoViewRouteWrapper.dart';
 import 'package:projects/style/textStyles.dart' as customStyles;
 import 'package:projects/style/unorderedListWidget.dart';
 
@@ -71,32 +72,4 @@ class UploadGuideFragment extends StatelessWidget {
   }
 }
 
-class HeroPhotoViewRouteWrapper extends StatelessWidget {
-  const HeroPhotoViewRouteWrapper({
-    required this.imageProvider,
-    this.backgroundDecoration,
-    this.minScale,
-    this.maxScale,
-  });
 
-  final ImageProvider imageProvider;
-  final BoxDecoration? backgroundDecoration;
-  final dynamic minScale;
-  final dynamic maxScale;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(
-        height: MediaQuery.of(context).size.height,
-      ),
-      child: PhotoView(
-        imageProvider: imageProvider,
-        backgroundDecoration: backgroundDecoration,
-        minScale: minScale,
-        maxScale: maxScale,
-        heroAttributes: const PhotoViewHeroAttributes(tag: "someTag"),
-      ),
-    );
-  }
-}
