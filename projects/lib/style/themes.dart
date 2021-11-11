25eeb9cb5dc277de6974833757d442d3cef0414f
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2021-11-11 14:52:41 +0100

    accent and secondary colors

--- projects/lib/style/themes.dart
+++ projects/lib/style/themes.dart
@@ -3,12 +3,20 @@ import 'package:flutter/material.dart';
 final ThemeData lightTheme = new ThemeData.from(colorScheme: ColorScheme.light(
   brightness: Brightness.light,
   primary: CustomColors.WIKIMEDIA_THEME_COLOR,
+
+  secondary: Color.fromRGBO(151, 0, 0, 1.0),
+
+  background: Color.fromRGBO(244, 244, 244, 1.0),
+  surface: Color.fromRGBO(252, 252, 252, 1.0),
+
 ));
 
 final ThemeData darkTheme = new ThemeData.from(colorScheme: ColorScheme.dark(
   brightness: Brightness.dark,
   primary: CustomColors.WIKIMEDIA_THEME_COLOR,
-  secondary: CustomColors.WIKIMEDIA_THEME_COLOR,
+
+  secondary: Color.fromRGBO(151, 0, 0, 1.0),
+
   background: Color.fromRGBO(28, 34, 39, 1.0),
   surface: Color.fromRGBO(35, 44, 51, 1.0),
 )).copyWith(
