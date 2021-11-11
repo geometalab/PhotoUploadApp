3069b3b096ea83fc61554ef1af793011ea13ead9
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2021-11-11 14:21:10 +0100

    better colors for dark mode

--- projects/lib/style/themes.dart
+++ projects/lib/style/themes.dart
@@ -9,7 +9,10 @@ final ThemeData darkTheme = new ThemeData.from(colorScheme: ColorScheme.dark(
   brightness: Brightness.dark,
   primary: CustomColors.WIKIMEDIA_THEME_COLOR,
   secondary: CustomColors.WIKIMEDIA_THEME_COLOR,
+  background: Color.fromRGBO(28, 34, 39, 1.0),
+  surface: Color.fromRGBO(35, 44, 51, 1.0),
 )).copyWith(
+  primaryColor: CustomColors.WIKIMEDIA_THEME_COLOR,
   appBarTheme: AppBarTheme(
     color: CustomColors.WIKIMEDIA_THEME_COLOR,
   )
