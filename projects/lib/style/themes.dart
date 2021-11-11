218eb94a78032e24506d6191692dcc68a2c92751
underwhelmingToaster <65400197+underwhelmingToaster@users.noreply.github.com>
2021-11-11 14:15:00 +0100

    updated dark and light theme with wikimedia colors

--- projects/lib/style/themes.dart
+++ projects/lib/style/themes.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-final ThemeData lightTheme = new ThemeData.light().copyWith();
-final ThemeData darkTheme = new ThemeData.dark().copyWith();
+final ThemeData lightTheme = new ThemeData.from(colorScheme: ColorScheme.light(
+  brightness: Brightness.light,
+  primary: CustomColors.WIKIMEDIA_THEME_COLOR,
+));
+
+final ThemeData darkTheme = new ThemeData.from(colorScheme: ColorScheme.dark(
+  brightness: Brightness.dark,
+  primary: CustomColors.WIKIMEDIA_THEME_COLOR,
+  secondary: CustomColors.WIKIMEDIA_THEME_COLOR,
+)).copyWith(
+  appBarTheme: AppBarTheme(
+    color: CustomColors.WIKIMEDIA_THEME_COLOR,
+  )
+);
 
 class CustomColors {
   static const WARNING_COLOR = Colors.amber;
   static const NO_IMAGE_COLOR = Colors.grey;
   static const NO_IMAGE_CONTENTS_COLOR = Colors.white70;
+  static const WIKIMEDIA_THEME_COLOR = Color.fromRGBO(0, 99, 152, 1.0);
 }
